56259752108823c9f2ab3c203ee60179455471e7
Georgi200031 <77017548+Georgi200031@users.noreply.github.com>
2022-11-24 16:24:30 +0200

    Update zadacha
    update

--- zadacha.cpp
+++ zadacha.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
-int a[100000];
+int a[100002];
 
 int solve(int N, int K){
       int sum=0, l, sal=0, d, ans;
       for(int i=0;i<N;i++)
             sum+=a[i];
       d=sum/K;
+      int sall = 1000000;
+      for(int j=0;j<N;j++){
       for(int i=0;i<N;i++){
             if(sal+a[i]<=d*(K-1)) {
                   sal+=a[i];
             }
+      }
+      if(sall>sal){
+      sall = sal;
+      }
       }
       ans=sum-sal;
       return ans;
